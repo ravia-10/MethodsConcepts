6488dde1d0243da4eeceb3f9b2d7151dec01a3f9
Ravi A <89862619+ravia-10@users.noreply.github.com>
2021-09-15 09:53:33 +0530

    Add files via upload

---
+++ Amazon.java
@@ -0,0 +1,27 @@
+package com.xworkz.methods;
+
+public class Amazon {
+
+	public static void Order() {
+		System.out.println("order placed successfull :)");
+	}
+
+	public static void addToCart() {
+		System.out.println("items are added to Cart ......");
+	}
+
+	public static void paymentmethod() {
+		System.out.println("you can to payment by Phonepay,amazon pay,g-pay...");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Login to Amazon is done successfully :)");
+		System.out.println();
+		Amazon.Order();
+		Amazon.addToCart();
+		Amazon.paymentmethod();
+		System.out.println();
+		System.out.println("logged out....");
+	}
+
+}
